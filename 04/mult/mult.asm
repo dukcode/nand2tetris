72b2b76f064de66dea505d8e8d6fd9dc0b517a38
dukcode <duk9741@gmail.com>
2023-10-29 05:38:08 +0900

    project04: 두 수 곱셈 어셈블리 코드 구현

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,4 +7,55 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// RAM[2] = 0
+// n = RAM[1]
+// for (int i = 0; i < n; ++i) {
+//     RAM[2] += RAM[0];
+// }
+
+// RAM[2] = 0
+// n = RAM[1]
+// i = 0
+// 
+// LOOP:
+//     if i==RAM[1] goto END
+//     RAM[2] = RAM[2] + RAM[0]
+//     i = i + 1
+//     goto LOOP
+//
+// END:
+//     goto END
+
+@R2
+M=0 // RAM[2] = 0
+
+@R1
+D=M
+@n
+M=D // i = RAM[1]
+
+@i
+M=0 // i = 0
+
+(LOOP)
+@n
+D=M
+@i
+D=D-M
+@END
+D;JEQ // if i==RAM[1] goto END
+
+@R0
+D=M
+@R2
+M=M+D // RAM[2] = RAM[2] + RAM[0]
+
+@i
+M=M+1 // i = i + 1;
+
+@LOOP
+0;JEQ // goto LOOP
+
+(END)
+@END
+0;JMP
