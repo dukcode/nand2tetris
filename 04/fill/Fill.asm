98c09d1919c967bc3dac4f031d90e82cbf8e8df1
dukcode <duk9741@gmail.com>
2023-10-30 15:26:04 +0900

    project04: 버튼 입력 시 스크린 채우기 코드 구현

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,87 @@
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-//// Replace this comment with your code.
+// LOOP:
+//     i = 0
+//     n = 256 * 512 / 16 = 8192
+//     if KBD == 0 goto BLACK
+//     else goto WHITE
+//
+//     BLACK:
+//         if i == n goto LOOP
+//         RAM[SCREEN + i] = -1
+//         i = i + 1
+//         goto BLACK
+//     goto LOOP
+//     WHITE:
+//         if i == n goto LOOP
+//         RAM[SCREEN + i] = -1
+//         i = i + 1
+//         goto WHITE
+//     goto LOOP
+
+
+(LOOP)
+@i
+M=0 // i = 0
+
+@8192
+D=A
+@n
+M=D // n = 8192
+
+@KBD
+D=M
+
+@BLACK
+D;JGT
+
+@WHITE
+0;JMP
+
+
+(BLACK)
+@n
+D=M
+@i
+D=D-M
+@LOOP
+D;JEQ // if i == n goto LOOP
+
+@SCREEN
+D=A
+@i
+A=D+M
+M=-1 // RAM[SCREEN + i] = fill
+
+@i
+M=M+1 // i = i + 1
+
+@BLACK
+0;JMP // goto FILL_SCREEN_LOOP
+
+@LOOP
+0;JMP // goto LOOP
+
+(WHITE)
+@n
+D=M
+@i
+D=D-M
+@LOOP
+D;JEQ // if i == n goto LOOP
+
+@SCREEN
+D=A
+@i
+A=D+M
+M=0 // RAM[SCREEN + i] = 0
+
+@i
+M=M+1 // i = i + 1
+
+@WHITE
+0;JMP // goto FILL_SCREEN_LOOP
+
+@LOOP
+0;JMP // goto LOOP
